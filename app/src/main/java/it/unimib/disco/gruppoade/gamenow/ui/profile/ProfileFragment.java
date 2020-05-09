29b30e3ff7347b343cb5d77d0eaa92a4deb59a9f
Perego Daniele <peregopr@gmail.com>
2020-05-09 10:49:05 +0200

    Aggiunta creazione delle chips
    Creazione runtime delle chip in base a un array utilizzato per testare il funzionamento
    Associato funzionamento al tasto x delle chips

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/ui/profile/ProfileFragment.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/ui/profile/ProfileFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import it.unimib.disco.gruppoade.gamenow.R;
@@ -32,29 +33,28 @@ public class ProfileFragment extends Fragment {
 
         if (tags != null)
             for (String text : tags) {
-                /*Chip chip = (Chip) inflater.inflate(R.layout.chip_item, container, false);
-                chip.setText(text.toUpperCase());
+                // creo la chips
+                Chip chip = new Chip(getContext());
+                chip.setText(text);
+                chip.setCloseIconVisible(true);
+                chip.setCheckable(false);
+                chip.setClickable(false);
+
+                // associo alla x la rimozione
                 chip.setOnCloseIconClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Chip tp_chip = (Chip) v;
-                        String tagDaEliminare = (String) tp_chip.getText();
                         chipGroup.removeView(v);
-                */
-                        // TODO Leggere da file il tag eliminato ed eliminarlo
-                  /*  try {
-                        fileReader.eliminaTag(tagDaEliminare);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }*/
-
-
                     }
-        // });
+                });
 
-                //add to group
-        //chipGroup.addView(chip);
-        //   }
+
+                // aggiunta chips alla chipsvgroup
+                chipGroup.addView(chip);
+                chipGroup.setVisibility(view.getVisibility());
+
+
+            }
 
 
         return view;
