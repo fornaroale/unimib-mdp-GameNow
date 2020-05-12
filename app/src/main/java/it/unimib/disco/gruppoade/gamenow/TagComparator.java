4acb7d6e1a72e38b43ad86186bed931797805df3
Perego Daniele <peregopr@gmail.com>
2020-05-12 10:16:15 +0200

    Rimosso pop-up, sostituita con Snackbar
    Rimossa la finestra di pop-up e sostituita con una snackbar che implementa la funzione UNDO per ripristinare un tags appena eliminato.
    Sostituito arrai di prova con una List, per implementare il sorting.
    Aggiunta funzione di formattazione del testo dei tags.

---
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/TagComparator.java
@@ -0,0 +1,19 @@
+package it.unimib.disco.gruppoade.gamenow;
+
+import java.util.Comparator;
+
+public class TagComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1 != null && o2 != null) {
+            o1 = o1.toLowerCase();
+            o2 = o2.toLowerCase();
+            return o1.compareTo(o2);
+        }
+
+
+        return 0;
+
+    }
+}
