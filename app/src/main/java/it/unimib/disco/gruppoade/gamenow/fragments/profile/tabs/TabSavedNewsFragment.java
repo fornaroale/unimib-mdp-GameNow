f337306b6f4d4805c53a3a2c0f897a104057a934
Alessandro <fornaroale@outlook.com>
2020-05-28 16:39:17 +0200

    Implemented basic and local saving news system

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/profile/tabs/TabSavedNewsFragment.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/profile/tabs/TabSavedNewsFragment.java
@@ -6,10 +6,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.threeten.bp.LocalDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.unimib.disco.gruppoade.gamenow.R;
+import it.unimib.disco.gruppoade.gamenow.adapters.SavedNewsListAdapter;
+import it.unimib.disco.gruppoade.gamenow.models.NewsProvider;
+import it.unimib.disco.gruppoade.gamenow.models.PieceOfNews;
 
 public class TabSavedNewsFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private SavedNewsListAdapter adapter;
+    private List<PieceOfNews> mSavedNewsModelList;
 
     public TabSavedNewsFragment() {
     }
@@ -24,6 +38,50 @@ public class TabSavedNewsFragment extends Fragment {
                              ViewGroup container,
                              Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_tab_saved_news, container, false);
+
+        // Recupero il recyclerview dal layout xml
+        mRecyclerView = root.findViewById(R.id.recyclerView);
+
+        mSavedNewsModelList = new ArrayList<>();
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setHasFixedSize(true);
+        adapter = new SavedNewsListAdapter(getActivity(), mSavedNewsModelList);
+        mRecyclerView.setAdapter(adapter);
+
+        // TODO: Carico notizie salvate (per ora locale:)
+        mSavedNewsModelList.add(new PieceOfNews("News salvata",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "https://google.it/", LocalDateTime.now(),
+                "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg",
+                "23544645764657845734563465",
+                new NewsProvider("EuroGamer",
+                        "https://www.eurogamer.it/",
+                        "https://www.eurogamer.it/?format=rss&platform=PS4",
+                        "PS4")));
+
+        mSavedNewsModelList.add(new PieceOfNews("News salvata",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "https://google.it/", LocalDateTime.now(),
+                "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg",
+                "23544645764657845734563465",
+                new NewsProvider("EuroGamer",
+                        "https://www.eurogamer.it/",
+                        "https://www.eurogamer.it/?format=rss&platform=PS4",
+                        "XBOX")));
+
+        mSavedNewsModelList.add(new PieceOfNews("News salvata",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "https://google.it/", LocalDateTime.now(),
+                "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg",
+                "23544645764657845734563465",
+                new NewsProvider("EuroGamer",
+                        "https://www.eurogamer.it/",
+                        "https://www.eurogamer.it/?format=rss&platform=PS4",
+                        "SWITCH")));
+
+        // Carico notizie nel RecyclerView
+        adapter.notifyDataSetChanged();
 
         return root;
     }
