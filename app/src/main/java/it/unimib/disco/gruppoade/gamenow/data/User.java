005c9c35c7b15e84dec548a0fd20b823fc3e537a
Perego Daniele <peregopr@gmail.com>
2020-05-28 17:37:27 +0200

    Creato oggetto User che viene salvato su DB, contenete le informazioni base compreso l'elenco di tag. Collegati le 4 checkbox relative alle piattaforme

---
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/data/User.java
@@ -0,0 +1,62 @@
+package it.unimib.disco.gruppoade.gamenow.data;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@IgnoreExtraProperties
+public class User {
+
+    public String username;
+    public String email;
+    public List<String> tags;
+
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        tags = new ArrayList<>();
+    }
+
+    public void addTag(String tag){
+        tags.add(tag);
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
+}
