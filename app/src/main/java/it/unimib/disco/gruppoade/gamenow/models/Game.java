0b3a68964f044f2e15fb7183544962a76f7c256a
edoardo-tarnoczy <ghjklop'09>
2020-06-07 16:38:00 +0200

    Replace GameInfoActivity with GameInfoFragment

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/models/Game.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/models/Game.java
@@ -1,12 +1,15 @@
 package it.unimib.disco.gruppoade.gamenow.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Comparator;
 import java.util.List;
 
-public class Game {
+public class Game implements Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -43,6 +46,37 @@ public class Game {
     @SerializedName("platforms")
     @Expose
     private List<Platform> platforms;
+
+    protected Game(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readInt();
+        }
+        rating = in.readDouble();
+        videos = in.createTypedArrayList(Video.CREATOR);
+        name = in.readString();
+        summary = in.readString();
+        storyline = in.readString();
+        if (in.readByte() == 0) {
+            date = null;
+        } else {
+            date = in.readInt();
+        }
+        platforms = in.createTypedArrayList(Platform.CREATOR);
+    }
+
+    public static final Creator<Game> CREATOR = new Creator<Game>() {
+        @Override
+        public Game createFromParcel(Parcel in) {
+            return new Game(in);
+        }
+
+        @Override
+        public Game[] newArray(int size) {
+            return new Game[size];
+        }
+    };
 
     public List<Platform> getPlatforms() {
         return platforms;
@@ -116,4 +150,45 @@ public class Game {
         this.videos = videos;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(id);
+        }
+        dest.writeDouble(rating);
+        dest.writeTypedList(videos);
+        dest.writeString(name);
+        dest.writeString(summary);
+        dest.writeString(storyline);
+        if (date == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(date);
+        }
+        dest.writeTypedList(platforms);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", rating=" + rating +
+                ", videos=" + videos +
+                ", name='" + name + '\'' +
+                ", summary='" + summary + '\'' +
+                ", storyline='" + storyline + '\'' +
+                ", cover=" + cover +
+                ", date=" + date +
+                ", platforms=" + platforms +
+                '}';
+    }
 }
