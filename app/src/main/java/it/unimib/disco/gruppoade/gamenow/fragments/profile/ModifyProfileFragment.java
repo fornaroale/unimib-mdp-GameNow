dadee8b2f1da19e5a950b4ebfcc395169c584a35
Perego Daniele <peregopr@gmail.com>
2020-06-10 14:17:01 +0200

    Aggiunta possibilità di scegliere nuova foto profilo, e riempito fragment con i dati dell'utente

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/profile/ModifyProfileFragment.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/profile/ModifyProfileFragment.java
@@ -1,25 +1,50 @@
 package it.unimib.disco.gruppoade.gamenow.fragments.profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import it.unimib.disco.gruppoade.gamenow.R;
+import it.unimib.disco.gruppoade.gamenow.database.FbDatabase;
 
 public class ModifyProfileFragment extends Fragment {
 
 
+    private static final int PICK_IMAGE_REQUEST = 234;
+
     private Button salvaModifiche;
+    private Button eliminaFoto;
+    private Button modificaFoto;
+    private EditText editUsername;
+    private CircleImageView profilePicture;
+
+    // dati
+    private File dbFile;
+    private Boolean eliminaFotoPremuto;
+    //a Uri object to store file path
+    private Uri filePath;
 
     public ModifyProfileFragment() {
         // Required empty public constructor
@@ -49,29 +74,133 @@ public class ModifyProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        eliminaFotoPremuto = false;
         salvaModifiche = getView().findViewById(R.id.btn_salva_modifiche);
+        editUsername = getView().findViewById(R.id.edit_username);
+
+        eliminaFoto = getView().findViewById(R.id.btn_delete);
+        modificaFoto = getView().findViewById(R.id.btn_change);
+
+        // setto la foto profilo con quella iniziale
+        setProfilePhoto();
+
+        // setto il testo dell'edit text con l'username corrente
+        editUsername.setText(FbDatabase.getUserAuth().getDisplayName());
+
+        // setto elimina foto
+        eliminaFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!eliminaFotoPremuto){
+                    eliminaFotoPremuto = true;
+                    filePath = null;
+                }
+
+            }
+        });
+
+        modificaFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+
+
         salvaModifiche.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
-//                CardView cv_info = getView().findViewById(R.id.cv_infoaccount);
-//                CardView cv_logout = getView().findViewById(R.id.cv_logout);
-//                CardView cv_elimina = getView().findViewById(R.id.cv_deleteaccount);
-//                CardView cv_topics = getView().findViewById(R.id.cv_topics);
-//
-//                cv_info.setVisibility(View.VISIBLE);
-//                cv_logout.setVisibility(View.VISIBLE);
-//                cv_elimina.setVisibility(View.VISIBLE);
-//                cv_topics.setVisibility(View.VISIBLE);
 
             }
         });
 
     }
 
+
+    private void delteProfilePhotoFromDB(){
+        // creo un riferimento allo storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference imagesRef = storage.getReference().child("images").child(FbDatabase.getUserAuth().getUid() + ".jpg");
+
+        // Delete the file
+        imagesRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                // File deleted successfully
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Uh-oh, an error occurred!
+            }
+        });
+    }
+
+    private void setProfilePhoto(){
+        profilePicture = getView().findViewById(R.id.profile_image_change);
+
+        // creo un riferimento allo storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference imagesRef = storage.getReference().child("images").child(FbDatabase.getUserAuth().getUid() + ".jpg");
+
+        dbFile = null;
+
+        try {
+            dbFile = File.createTempFile("images", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // se riesco a scaricare la foto profilo la mostro con picasso
+        imagesRef.getFile(dbFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                Picasso.get()
+                        .load(dbFile)
+                        .fit()
+                        .centerCrop()
+                        .into((CircleImageView) profilePicture);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+            }
+        });
+
+    }
+
+    //method to show file chooser
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == -1 && data != null && data.getData() != null) {
+
+            // prendo il persocroso della foto
+            filePath = data.getData();
+
+            // la stampo usando picasso
+            if(filePath != null){
+                Picasso.get()
+                        .load(filePath)
+                        .fit()
+                        .centerCrop()
+                        .into((CircleImageView) profilePicture);
+
+                profilePicture.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
