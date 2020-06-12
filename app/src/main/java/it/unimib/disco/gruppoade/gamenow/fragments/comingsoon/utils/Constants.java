be858178e3f9790d96a0f7f450ed25c427916c1c
edoardo-tarnoczy <edoardo.tarnoczy@yahoo.com>
2020-06-13 00:21:05 +0200

    Fix Button change state adding configChange in Manifest for Main Activity.

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/comingsoon/utils/Constants.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/comingsoon/utils/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static final String IGDB_API_KEY = "ab4f92a787f1b14954b8d3fb288dc8afbf1ed68d";
     public static final String BASE_URL = "https://api-v3.igdb.com/";
-    public static final int PAGE_SIZE = 10;
+    public static final int PAGE_SIZE = 20;
 
 }
