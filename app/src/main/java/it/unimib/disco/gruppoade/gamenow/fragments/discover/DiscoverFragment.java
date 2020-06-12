0f27438badc2124441a406f972ebb522f399e473
Alessandro <fornaroale@outlook.com>
2020-06-12 23:11:06 +0200

    Removed useless adapter class; minor fixes to layouts and strings

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/discover/DiscoverFragment.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/fragments/discover/DiscoverFragment.java
@@ -42,7 +42,7 @@ import java.util.Collections;
 import java.util.List;
 
 import it.unimib.disco.gruppoade.gamenow.R;
-import it.unimib.disco.gruppoade.gamenow.adapters.RssListAdapter;
+import it.unimib.disco.gruppoade.gamenow.adapters.NewsListAdapter;
 import it.unimib.disco.gruppoade.gamenow.database.FbDatabase;
 import it.unimib.disco.gruppoade.gamenow.models.NewsProvider;
 import it.unimib.disco.gruppoade.gamenow.models.PieceOfNews;
@@ -57,7 +57,7 @@ public class DiscoverFragment extends Fragment {
     private SwipeRefreshLayout mSwipeLayout;
     private List<PieceOfNews> mFeedModelList;
     private DiscoverViewModel discoverViewModel;
-    private RssListAdapter adapter;
+    private NewsListAdapter adapter;
     private User user;
 
     private ValueEventListener postListenerFirstUserData = new ValueEventListener() {
@@ -70,7 +70,7 @@ public class DiscoverFragment extends Fragment {
             LinearLayoutManager manager = new LinearLayoutManager(getActivity());
             mRecyclerView.setLayoutManager(manager);
             mRecyclerView.setHasFixedSize(true);
-            adapter = new RssListAdapter(getActivity(), mFeedModelList, user);
+            adapter = new NewsListAdapter(getActivity(), mFeedModelList, user, false);
             mRecyclerView.setAdapter(adapter);
 
             new ProcessInBackground().execute(readProvidersCsv());
