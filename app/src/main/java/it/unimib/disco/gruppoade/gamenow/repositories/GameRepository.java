8db1e05b8fbb4dba2f7127fde1d830d7f82bae7a
edoardo-tarnoczy <edoardo.tarnoczy@yahoo.com>
2020-06-13 17:33:29 +0200

    Finish Lazy Loading

--- app/src/main/java/it/unimib/disco/gruppoade/gamenow/repositories/GameRepository.java
+++ app/src/main/java/it/unimib/disco/gruppoade/gamenow/repositories/GameRepository.java
@@ -1,17 +1,12 @@
 package it.unimib.disco.gruppoade.gamenow.repositories;
 
-import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.gson.Gson;
 
 import java.util.List;
 
-import it.unimib.disco.gruppoade.gamenow.fragments.comingsoon.ComingSoonFragment;
 import it.unimib.disco.gruppoade.gamenow.fragments.comingsoon.utils.ApiClient;
 import it.unimib.disco.gruppoade.gamenow.fragments.comingsoon.utils.ApiInterface;
 import it.unimib.disco.gruppoade.gamenow.models.Game;
@@ -19,7 +14,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static android.view.View.GONE;
 
 public class GameRepository {
     //Classe Singleton
@@ -41,15 +35,19 @@ public class GameRepository {
     }
 
     public void getGames(final MutableLiveData<List<Game>> games, String body){
-        final Gson gson = new Gson();
 
         Call<List<Game>> call = apiInterface.getGames(body);
         call.enqueue(new Callback<List<Game>>() {
             @Override
             public void onResponse(Call<List<Game>> call, Response<List<Game>> response) {
                 if(response.isSuccessful() && response.body() != null){
-                    games.postValue(null);
-                    games.postValue(response.body());
+                    if(games.getValue() != null) {
+                        List<Game> currentGameList = games.getValue();
+                        currentGameList.remove(currentGameList.size() - 1);
+                        currentGameList.addAll(response.body());
+                        games.postValue(currentGameList);
+                    } else
+                        games.postValue(response.body());
                 }
             }
 
